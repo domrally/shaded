66604e140d1b45f7a91756c2afc5529631f5f190
Dom <dominicemandy@gmail.com>
2023-02-15 09:12:24 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ function getHtml(vertex: string, fragment: string) {
 				// onLoad callback
 				geometry => {
 					geometry.computeVertexNormals()
-					const object = new Mesh(new TorusKnotGeometry(1, .3, 300, 20), material)
+					const object = new Mesh(geometry, material)
 
 					scene.add(object)
 
